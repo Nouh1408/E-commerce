103abbdbe59f9801ad88fb79c4a750d3fb7b97e3
Nouh1408 <ahmedinouh@gmail.com>
2025-02-02 21:14:11 +0200

    some supdates

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@
 
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Register from './Components/Register/Register.jsx'
-import Login from './Components/Login/Login.jsx'
+import Home from './Components/Home/Home.jsx'
+import About from './Components/About/About.jsx'
+import Posts from './Components/Posts/Posts.jsx'
+import Layout from './Components/Layout/Layout.jsx'
 
 
 function App() {
 
   const router = createBrowserRouter([
-    {path:"register", element:<Register />},
-    {path:"login", element:<Login/>},
+    {path:"",element:<Layout />,children:[
+      {path:"home",element:<Home/>},
+      {path:"about",element:<About/>},
+      {path:"posts", element:<Posts/>},
+    ]}
   ])
+  
+  
   return (
      <RouterProvider router={router} />
     
