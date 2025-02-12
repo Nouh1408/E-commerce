d69f38ab4763b14bce6f6a7181fe9035883a40ce
Nouh1408 <ahmedinouh@gmail.com>
2025-02-12 21:47:40 +0200

    alerts for password and name

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 
 export default function Register() {
+  const [NameAlert, setNameAlert] = useState(false)
+  const [PassAlert, setPassAlert] = useState(false)
+  const [EmailAlert, setEmailAlert] = useState(false)
+  const [PhoneAlert, setPhoneAlert] = useState(false)
+  const [PassAlertCheck, setPassAlertCheck] = useState(false)
+
+  const handle =(e) =>{
+    if(!e.target.value){
+      setNameAlert(true)
+    } else{
+      setNameAlert(false)
+    }
+  }
+
+  const handle1 =(e) =>{
+    if(!e.target.value){
+      setPassAlert(true)
+  } else{
+    setPassAlert(false)
+  }
+
   return (
     <div className='container mx-auto px-4 mt-12'>
       <div className=" w-3/4">
@@ -14,7 +35,13 @@ export default function Register() {
          name="nameF"
           id="nameF"
           className="border border-gray-300 p-2 rounded-md w-full"
+          onBlur={handle}
           />
+          {
+          NameAlert && (<div className="p-4 mb-4 text-sm text-red-600 rounded-lg bg-red-200" role="alert">name is required</div>)
+          }
+
+
         <label htmlFor="emailF" className="text-lg font-medium">
           Email:
         </label>
@@ -34,8 +61,12 @@ export default function Register() {
           id="passF"
           name="passF"
           className="border border-gray-300 p-2 rounded-md w-full"
-  
+          onBlur={handle1}
         />
+        {
+          NameAlert && (<div className="p-4 mb-4 text-sm text-red-600 rounded-lg bg-red-200" role="alert">password is required</div>)
+        }
+
         <label htmlFor="re" className="text-lg font-medium">re-enter password :</label>
         <input type="password"
         name='re'
